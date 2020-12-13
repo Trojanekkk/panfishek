108c1650a21b44540c2d397d271cbc5d08f802ec
Wojciech Trojanowski <W.Trojan27@gmail.com>
2020-12-13 23:49:29 +0100

    Del: Axios plugin, Add: Vetru config file

--- src/components/Guess.vue
+++ src/components/Guess.vue
@@ -1,6 +1,6 @@
 <template>
     <div id="guess">
-        <p>Word: <b>{{ word }}</b><b-icon icon="x-circle" class="float-right" font-scale="0.5"></b-icon></p>
+        <p>Word: <b>{{ word }}</b><b-icon icon="x-circle" class="float-right" font-scale="0.5" shift-v="-8"></b-icon></p>
         <p>
             <b-form-input v-model="guess" placeholder="Enter translation"></b-form-input>
         </p>
@@ -12,13 +12,10 @@
                 </b-button-group>
             </b-button-toolbar>
         </p>
-        <!-- <p @click="getTodo">insert todo manually</p>
-        <p>{{ todo }}</p> -->
     </div>
 </template>
 
 <script>
-import axios from 'axios'
 
 export default {
     name: 'Guess',
@@ -26,22 +23,12 @@ export default {
         return {
             word: 'Naleśnik',
             translation: 'pancake',
-            guess: '',
-            todo: {},
-            todoo: {}
+            guess: ''
         }
     },
     methods: {
-        getTodo: function () {
-            return axios.get(
-                'https://jsonplaceholder.typicode.com/todos/1'
-            ).then((res) => { this.todo = res.data })
-        }
     },
     mounted () {
-        axios.get(
-            'https://jsonplaceholder.typicode.com/todos/1'
-        ).then((res) => { this.todoo = res.data })
     }
 }
 </script>
